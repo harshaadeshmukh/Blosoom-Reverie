c96be05363e6b908dd224ba66f64542445548a8c
harshaadeshmukh <harshaadeshmukh@users.noreply.github.com>
2026-06-21 01:28:09 +0530

    Fix build by removing Occasions import from Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero';
 import MarqueeStrip from '../components/MarqueeStrip';
 import Process from '../components/Process';
-import Occasions from '../components/Occasions';
 import Testimonials from '../components/Testimonials';
 import CtaBand from '../components/CtaBand';
 
@@ -11,7 +10,6 @@ export default function Home() {
       <Hero />
       <MarqueeStrip />
       <Process />
-      <Occasions />
       <Testimonials />
       <CtaBand />
     </main>
